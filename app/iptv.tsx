a5316043cdfc564426126e20756fd3d51922e1d7
denisguaira <denisguaira@googlemail.com>
2025-07-19 15:27:48 +1200

    Create iptv.tsx

---
+++ app/iptv.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function IPTVLogin() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>📺 IPTV Login Screen</Text>
+    </View>
+  );
+}
