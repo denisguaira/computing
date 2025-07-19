56d19a82919c7631e82f036fd69b6ccaa1f782d8
denisguaira <denisguaira@googlemail.com>
2025-07-19 15:21:42 +1200

    Update iptv.tsx

--- iptv.tsx
+++ iptv.tsx
@@ -1,20 +1,9 @@
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function IPTVScreen() {
   return (
-    <View style={{ padding: 20, flex: 1 }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>IPTV Login</Text>
-
-      <Text>Server URL</Text>
-      <TextInput style={{ borderWidth: 1, marginBottom: 10 }} />
-
-      <Text>Username</Text>
-      <TextInput style={{ borderWidth: 1, marginBottom: 10 }} />
-
-      <Text>Password</Text>
-      <TextInput style={{ borderWidth: 1, marginBottom: 10, marginBottom: 20 }} secureTextEntry />
-
-      <Button title="Connect" onPress={() => {}} />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>📺 IPTV Setup Screen</Text>
     </View>
   );
 }
