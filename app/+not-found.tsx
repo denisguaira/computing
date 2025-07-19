fcfda685c7799d073ef761fc5f8c1305d2e506c9
Denis Lima <denisguaira@googlemail.com>
2025-07-19 12:19:18 +1200

    Initial commit

---
+++ app/+not-found.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <ThemedView style={styles.container}>
+        <ThemedText type="title">This screen does not exist.</ThemedText>
+        <Link href="/" style={styles.link}>
+          <ThemedText type="link">Go to home screen!</ThemedText>
+        </Link>
+      </ThemedView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
+import { Link } from 'expo-router'; // at the top
+
+// Inside return()
+<Link href="/iptv">
+  <ThemedText type="link">➡ Go to IPTV Setup</ThemedText>
+</Link>
